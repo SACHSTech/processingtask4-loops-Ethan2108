2728695e876f2382415e2160a7e4c13ebeb103a3
Ethan2108 <ethan.au23@ycdsbk12.ca>
2022-03-22 21:38:44 -0400

    deleted more unneccessary code

--- Sketch.java
+++ Sketch.java
@@ -7,17 +7,10 @@ public class Sketch extends PApplet {
     size(500, 500);
   }
 
-  /** 
-   * Called once at the beginning of execution.  Add initial set up
-   * values here i.e background, stroke, fill etc.
-   */
   public void setup() {
     background(255, 255, 255);
   }
 
-  /**
-   * Called repeatedly, anything drawn to the screen goes here
-   */
   public void draw() {
 	  
 	
